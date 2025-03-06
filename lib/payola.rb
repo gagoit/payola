7b9afc954bb2a27a09555907a04f0391566ea625
Dat Pham-Ba <dat_phamba1307@yahoo.com>
2025-03-06 21:41:06 +0700

    Update payola.rb

--- lib/payola.rb
+++ lib/payola.rb
@@ -35,7 +35,7 @@ module Payola
 
     def configure(&block)
       raise ArgumentError, "must provide a block" unless block_given?
-      block.arity.zero? ? instance_eval(&block) : yield(self)
+      block.parameters.empty? ? instance_eval(&block) : yield(self)
     end
 
     def secret_key_for_sale(sale)
